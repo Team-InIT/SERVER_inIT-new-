b6fc6d321c7bf6c8d7987b8cff5bf5fcac24d9ca
Jang Yoon Jung <sj87at@gmail.com>
2022-11-13 21:40:53 +0900

    [FEAT]#3 S3 bucket connection test (ing)

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
-const cookieParser = reqire('cookie-parser');
+const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const dotenv = reqire('dotenv');
+const path = require('path');
+const dotenv = require('dotenv');
+const imageUploader = require('./imageUploader');
 
 dotenv.config();
 const app = express();
+
 //포트 번호 3006으로 기본값 설정
 app.set('port', process.env.PORT || 3006);
 
@@ -25,6 +28,25 @@ app.use(session({
     name: 'session-cookie',
 }));
 
+
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 서버 대기 중');
+});
+
+//서버 동작 테스트
+app.post('/test', async function (req,res) {
+    console.log('test api')
+    res.json({
+        'message': "테스트 완료, 서버 동작 확인"
+    });
+});
+
+//s3 연동 테스트
+app.post('/img', imageUploader.single("img"), (req,res,next) => {
+    console.log(req.file);
+    console.log(process.env.S3_ACCESS_KEY_ID);
+    console.log(process.env.S3_SECRET_ACCESS_KEY);
+    res.json({
+        "url": req.file.location, //이미지 파일 경로
+    });
 });
